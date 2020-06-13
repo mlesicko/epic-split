b4d4be9ba49ee8691ab9b4970682bc26c74e3077
C. Warren Dale <adventureonthehighseas@gmail.com>
2020-06-13 14:51:29 -0500

    Migrate to state

--- app/main.rb
+++ app/main.rb
@@ -1,29 +1,35 @@
+def decenter sprite
+  return [sprite.x - sprite.w / 2, sprite.y - sprite.h / 2]
+end
+
 def tick args
   args.outputs.labels << [ 580, 500, 'Hello World!']
   args.outputs.labels << [ 475, 150, '(Consider reading README.txt now.)']
 
-  truckw = 88
-  truckh = 218
-
-  truck1x = 520
-  truck1y = 310
-  truck2x = 650
-  truck2y = 310
-  jcvdw = 52
-  jcvdh = 36
-  jcvdx = ((truck1x + truck2x) / 2)
-  jcvdy = ((truck1y + truck2y) / 2)
+  args.state.trucks.left.w ||= 88
+  args.state.trucks.left.h ||= 218
+  args.state.trucks.right.w ||= 88
+  args.state.trucks.right.h ||= 218
+  
+  args.state.trucks.left.x ||= 520
+  args.state.trucks.left.y ||= 310
+  args.state.trucks.right.x ||= 650
+  args.state.trucks.right.y ||= 310
+  args.state.jcvd.w ||= 52
+  args.state.jcvd.h ||= 36
+  args.state.jcvd.x = ((args.state.trucks.left.x + args.state.trucks.right.x) / 2)
+  args.state.jcvd.y = ((args.state.trucks.left.y + args.state.trucks.right.y) / 2)
 
   if args.inputs.keyboard.key_down.w
     args.state.h_pressed_at = args.state.tick_count
   end
 
-  truck1 = [ truck1x - truckw/2 , truck1y - truckh/2, truckw, truckh, 'sprites/truck.png' ]
-  truck2 = [ truck2x - truckw/2, truck2y - truckh/2, truckw, truckh, 'sprites/truck.png' ]
-  jcvd = [ jcvdx - jcvdw/2, jcvdy - jcvdh/2, 52, 36, 'sprites/character_black_white.png', 180]
+  truck_left = [ * (decenter args.state.trucks.left), args.state.trucks.left.w, args.state.trucks.left.h, 'sprites/truck.png' ]
+  truck_right = [ * (decenter args.state.trucks.right), args.state.trucks.right.w, args.state.trucks.right.h, 'sprites/truck.png' ]
+  jcvd = [ * (decenter args.state.jcvd), 52, 36, 'sprites/character_black_white.png', 180]
   
-  args.outputs.sprites << truck1
-  args.outputs.sprites << truck2
+  args.outputs.sprites << truck_left
+  args.outputs.sprites << truck_right
   args.outputs.sprites << jcvd
 
 end
