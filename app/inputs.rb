35160ea28020beb4cb0ff14cc071a3376ceff72d
Matt <loggerisms@gmail.com>
2020-06-13 22:34:26 -0500

    truck move diagonally

--- app/inputs.rb
+++ app/inputs.rb
@@ -5,14 +5,14 @@ def handleInputs args
   xspeed = 2
 
 
-  if args.inputs.keyboard.key_held.w || args.inputs.controller_one.left_analog_y_perc > 0.5
+  if args.inputs.keyboard.key_held.w || args.inputs.controller_one.left_analog_y_perc >= 0.5
     left.y += yspeed
     if truck_hit_truck? left, right or (left.y + left.h/2 > args.state.screen.h)
       left.y -= yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.s || args.inputs.controller_one.left_analog_y_perc < -0.5
+  if args.inputs.keyboard.key_held.s || args.inputs.controller_one.left_analog_y_perc <= -0.5
     left.y -= yspeed
     if (truck_hit_truck? left, right)
       left.y += yspeed
@@ -20,14 +20,14 @@ def handleInputs args
 
   end
 
-  if args.inputs.keyboard.key_held.a || args.inputs.controller_one.left_analog_x_perc < -0.5
+  if args.inputs.keyboard.key_held.a || args.inputs.controller_one.left_analog_x_perc <= -0.5
     left.x -= xspeed
     if (truck_hit_truck? left, right) or (left.x - left.w/2 < 0)
       left.x += xspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.d || args.inputs.controller_one.left_analog_x_perc > 0.5
+  if args.inputs.keyboard.key_held.d || args.inputs.controller_one.left_analog_x_perc >= 0.5
     left.x += xspeed
     if (truck_hit_truck? left, right) or (left.x + left.w/2 > args.state.screen.w)
       left.x -= xspeed
@@ -35,28 +35,28 @@ def handleInputs args
   end
 
 
-  if args.inputs.keyboard.key_held.up || args.inputs.controller_one.right_analog_y_perc > 0.5
+  if args.inputs.keyboard.key_held.up || args.inputs.controller_one.right_analog_y_perc >= 0.5
     right.y += yspeed
     if (truck_hit_truck? right, left) or (right.y + right.h/2 > args.state.screen.h)
       right.y -= yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.down || args.inputs.controller_one.right_analog_y_perc < -0.5
+  if args.inputs.keyboard.key_held.down || args.inputs.controller_one.right_analog_y_perc <= -0.5
     right.y -= yspeed
     if truck_hit_truck? right, left
       right.y += yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.left || args.inputs.controller_one.right_analog_x_perc < -0.5
+  if args.inputs.keyboard.key_held.left || args.inputs.controller_one.right_analog_x_perc <= -0.5
     right.x -= xspeed
     if (truck_hit_truck? right, left) or (right.x - right.w/2 < 0)
       right.x += xspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.right || args.inputs.controller_one.right_analog_x_perc > 0.5
+  if args.inputs.keyboard.key_held.right || args.inputs.controller_one.right_analog_x_perc >= 0.5
     right.x += xspeed
     if (truck_hit_truck? right, left) or (right.x + right.w/2 > args.state.screen.w)
       right.x -= xspeed
