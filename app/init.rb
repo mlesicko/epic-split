bbc99ab0c8462de8cd4e8b92d23d16cc70f3942e
C. Warren Dale <adventureonthehighseas@gmail.com>
2020-06-13 19:20:37 -0500

    Precise starting point

--- app/init.rb
+++ app/init.rb
@@ -4,9 +4,9 @@ def init_trucks args
   args.state.trucks.right.w ||= 88
   args.state.trucks.right.h ||= 218
   
-  args.state.trucks.left.x ||= 520
+  args.state.trucks.left.x = 575
   args.state.trucks.left.y ||= 310
-  args.state.trucks.right.x ||= 650
+  args.state.trucks.right.x = 705
   args.state.trucks.right.y ||= 310
 end
 
@@ -17,16 +17,16 @@ def init_tiles args
   args.state.tile.h ||= 128
   args.state.tile.scroll.y ||= 0
   
-  tile_count_x = (args.state.screen.w / args.state.tile.w).to_i
-  tile_count_y = (args.state.screen.h / args.state.tile.h).to_i 
-  tiles_x = (0..tile_count_x).to_a
-  tiles_y = (0..tile_count_y + 1).to_a
+  args.state.tile_count_x = (args.state.screen.w / args.state.tile.w).to_i
+  args.state.tile_count_y = (args.state.screen.h / args.state.tile.h).to_i 
+  tiles_x = (0..args.state.tile_count_x).to_a
+  tiles_y = (0..args.state.tile_count_y + 1).to_a
   args.state.tiles ||= tiles_x.map do |x| 
     tiles_y.map do |y| 
       args.state.new_entity(:tile,
-                       { x: (args.state.tile.w * x),
-                         y: (args.state.tile.h * y) - args.state.tile.scroll.y,
-                         img: "sprites/tiles/grass/land_grass11.png"})
+                            { x: (args.state.tile.w * x),
+                              y: (args.state.tile.h * y) - args.state.tile.scroll.y,
+                              surface: choose_surface(args, x, y)})
     end
   end
 
--- app/tiling.rb
+++ app/tiling.rb
@@ -1,7 +1,24 @@
+def choose_surface args, x, y
+  if (x * 2 == args.state.tile_count_x) or (x * 2 == args.state.tile_count_x - 2)
+    return "road"
+  else
+    return "grass"
+  end
+end
+
+def image_by_surface surface_type
+  case surface_type  
+  when "grass"
+    return "sprites/tiles/grass/land_grass11.png"
+  when  "road"
+    return "sprites/tiles/asphalt\ road/road_asphalt01.png"
+  end
+end
+
 def draw_map args
   bg = args.state.tiles.map do |r| 
      r.map do |t| 
-       [t.x, t.y - args.state.tile.scroll.y, 128, 128, t.img]
+       [t.x, t.y - args.state.tile.scroll.y, 128, 128, (image_by_surface t.surface)]
     end
   end
 
