d9ea48717dac10a9088b1074dca8889a9a53460a
Matt <loggerisms@gmail.com>
2020-06-13 22:19:21 -0500

    Controller support.

--- app/inputs.rb
+++ app/inputs.rb
@@ -5,14 +5,14 @@ def handleInputs args
   xspeed = 2
 
 
-  if args.inputs.keyboard.key_held.w
+  if args.inputs.keyboard.key_held.w || args.inputs.controller_one.left_analog_y_perc > 0.5
     left.y += yspeed
     if truck_hit_truck? left, right or (left.y + left.h/2 > args.state.screen.h)
       left.y -= yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.s
+  if args.inputs.keyboard.key_held.s || args.inputs.controller_one.left_analog_y_perc < -0.5
     left.y -= yspeed
     if (truck_hit_truck? left, right)
       left.y += yspeed
@@ -20,14 +20,14 @@ def handleInputs args
 
   end
 
-  if args.inputs.keyboard.key_held.a
+  if args.inputs.keyboard.key_held.a || args.inputs.controller_one.left_analog_x_perc < -0.5
     left.x -= xspeed
     if (truck_hit_truck? left, right) or (left.x - left.w/2 < 0)
       left.x += xspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.d
+  if args.inputs.keyboard.key_held.d || args.inputs.controller_one.left_analog_x_perc > 0.5
     left.x += xspeed
     if (truck_hit_truck? left, right) or (left.x + left.w/2 > args.state.screen.w)
       left.x -= xspeed
@@ -35,33 +35,32 @@ def handleInputs args
   end
 
 
-  if args.inputs.keyboard.key_held.up
+  if args.inputs.keyboard.key_held.up || args.inputs.controller_one.right_analog_y_perc > 0.5
     right.y += yspeed
     if (truck_hit_truck? right, left) or (right.y + right.h/2 > args.state.screen.h)
       right.y -= yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.down
+  if args.inputs.keyboard.key_held.down || args.inputs.controller_one.right_analog_y_perc < -0.5
     right.y -= yspeed
     if truck_hit_truck? right, left
       right.y += yspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.left
+  if args.inputs.keyboard.key_held.left || args.inputs.controller_one.right_analog_x_perc < -0.5
     right.x -= xspeed
     if (truck_hit_truck? right, left) or (right.x - right.w/2 < 0)
       right.x += xspeed
     end
   end
 
-  if args.inputs.keyboard.key_held.right
+  if args.inputs.keyboard.key_held.right || args.inputs.controller_one.right_analog_x_perc > 0.5
     right.x += xspeed
     if (truck_hit_truck? right, left) or (right.x + right.w/2 > args.state.screen.w)
       right.x -= xspeed
     end
   end
-
 
 end
